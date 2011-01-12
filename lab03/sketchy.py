211fa65e41c460c9bf2731881ba6faee39f53be6
I. H. McCreery <ihmccreery@gmail.com>
2011-01-12 13:39:18 -0500

    changed lab03/sketchy.py slightly.

--- lab03/sketchy.py
+++ lab03/sketchy.py
@@ -4,13 +4,17 @@
 # I. H. McCreery
 # 10 January 2010
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageTk
+import Tkinter
 
-CANVAS_SIZE = (800, 800)
+CANVAS_SIZE = (400, 400)
 
 def main():
     # Assign/initialize image
-    im = Image.new('RGB', CANVAS_SIZE, (255, 255, 255))
+    im = Image.new('RGB', CANVAS_SIZE, (127, 0, 0))
+    draw = ImageDraw.Draw(im)
+    draw.chord((20, 20, 40, 40), 0, 180, (0, 0, 0))
+    draw.chord((90, 20, 180, 100), 0, 180, (0, 0, 0))
     im.show()
 
 main()
