b43f6be477e574a23e4b19ebacf911de2d1e8518
I. H. McCreery <ihmccreery@gmail.com>
2011-01-20 13:17:19 -0600

    More messing around with Picture stuff. It's unclear to me how we're going to get a picture to display in Tk then refresh.

--- lab03/ihm/sketchy.py
+++ lab03/ihm/sketchy.py
@@ -1,6 +1,6 @@
 # sketchy.py
 # draws a picture and saves it to the current directory
-# 
+#
 # I. H. McCreery
 # 10 January 2010
 
@@ -11,10 +11,20 @@ CANVAS_SIZE = (400, 400)
 
 def main():
     # Assign/initialize image
-    im = Image.new('RGB', CANVAS_SIZE, (127, 0, 0))
-    draw = ImageDraw.Draw(im)
-    draw.chord((20, 20, 40, 40), 0, 180, (0, 0, 0))
-    draw.chord((90, 20, 180, 100), 0, 180, (0, 0, 0))
+    im = image('RGB', CANVAS_SIZE, (127, 0, 0))
+    # im.draw.ellipse([(50, 50), (100, 100)], outline=(255, 255, 255), fill=(0, 0, 0))
     im.show()
+
+class image(Image.Image):
+
+    def __init__(self, mode, size, color):
+        self = Image.new(mode, size, color)
+        self.root = Tkinter.Tk()
+        self.draw = ImageDraw.Draw(self)
+        self.tk_label_im = None
+
+    def display(self):
+        tk_im = ImageTk.PhotoImage(self)
+        self.tk_label_im = Tkinter.Label(self.root, image=tk_im)
 
 main()
